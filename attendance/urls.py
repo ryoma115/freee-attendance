8862934a5b7df96878b42637f067248c426c51d1
ryoma115 <nakayamaryoma@gmail.com>
2022-04-15 01:33:48 +0900

    #10 完了

--- attendance/urls.py
+++ attendance/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'attendance'
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('result/', views.result, name='result'),
+    path('result/', views.result, name='result'),
 ]
