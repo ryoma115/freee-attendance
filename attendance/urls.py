95a25ee897a588de906cccad632076e5699b95cb
ryoma115 <nakayamaryoma@gmail.com>
2022-04-18 18:53:54 +0900

    #45 完了

--- attendance/urls.py
+++ attendance/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('result/', views.result, name='result'),
     path('lists/', views.attendancesList, name='list'),
+    path('<int:pk>/delete/', views.attendanceDelete, name='delete'),
 ]
